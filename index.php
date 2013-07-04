ea97fc667504285d7985069b81c27c17aa0f9be2
Angelo Tata <atata@ebay.com>
2013-07-04 14:28:43 +0200

    Split between configuration and functionality

--- index.php
+++ index.php
@@ -2,28 +2,33 @@
 <html>
     <head>
         <script type="text/javascript">
-            var sitespeed = {};
-            sitespeed.data = {};
-            sitespeed.data.sessionid = "harm-session";
-            sitespeed.data.pageid = "<?=uniqid();?>";
-            sitespeed.data.domStart = new Date().getTime();
-            sitespeed.currentTime = function() {
-                return new Date().getTime();
-            }
-            sitespeed.event = function(name) {
-                sitespeed.data.events = sitespeed.data.events || {};
-                sitespeed.data.events[name] = sitespeed.currentTime();
-            }
+            window.sitespeed = {};
+            window.sitespeed.domStart = new Date().getTime();
+            window.sitespeed.sessionId = 'harm-session';
+            window.sitespeed.pageId = '<?=uniqid();?>';
         </script>
     </head>
     <body>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+
         <script type="text/javascript">
-            var ts=new Date().getTime();
-            document.write("<sc"+"ript src=\"sitespeed.js?"+ts+"\" type=\"text/javascript\"></"+"script>"); 
+            var ts = new Date().getTime();
+            document.write("<sc"+"ript src=\"sitespeed.js?" + ts + "\" type=\"text/javascript\"></"+"script>");
         </script>
+
+        <script>
+            $(window).load(function() {
+                $.post(
+                    'sensor.php',
+                    window.sitespeed.collect()
+                ).done(function(response) {
+                    console.log("Data Saved: " + response);
+                });
+            });
+        </script>
+
         <a href="?<?=time()?>">Clickie</a>
-        <script>sitespeed.event("beforeBanner");</script>
-        <script>sitespeed.event("afterBanner");</script>
+        <script>window.sitespeed.event("beforeBanner");</script>
+        <script>window.sitespeed.event("afterBanner");</script>
     </body>
 </html>
